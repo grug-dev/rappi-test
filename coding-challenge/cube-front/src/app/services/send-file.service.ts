d3cf5f19ff61ee4dd6409c27f1c6002fbf1509da
kkpa <cristiancamilopena@gmail.com>
2018-01-14 20:44:53 -0500

    Removing http headers

--- coding-challenge/cube-front/src/app/services/send-file.service.ts
+++ coding-challenge/cube-front/src/app/services/send-file.service.ts
@@ -20,16 +20,7 @@ export class SendFileService {
     formData.append('file', fileItem.archivo);
     const url: string = environment.HTTP_URL + 'cube/upload';
 
-     const headers = new HttpHeaders(
-       {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-      'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
-      'Access-Control-Allow-Credentials': true
-    });
-
-
-      return this.http.post(url, formData);
+     return this.http.post(url, formData);
   }
 
 }
